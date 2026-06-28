b065b0878ba9eca64ea6863e7405691abdfa9988
Nandini <2419200539@onlinesmu.edu.in>
2026-06-28 17:06:09 +0530

    Table booking and menu API implementation

--- myproject/restaurant/urls.py
+++ myproject/restaurant/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('test/', views.index, name='test'),
+    path('menu/', views.menu, name='menu'),
+    path('menu/<int:pk>/', views.single_menu_item, name='single-menu-item'),
+    path('bookings/', views.bookings, name='bookings'),
+    path('bookings/<int:pk>/', views.booking, name='booking'),
 ]
--- myproject/restaurant/views.py
+++ myproject/restaurant/views.py
@@ -1,9 +1,112 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework import viewsets
+from .models import Booking
+from .serializers import BookingSerializer
+
 
 def home(request):
     return HttpResponse("Welcome to Restaurant")
+
+
 def index(request):
-    return render(request,'index.html',{})
+    return render(request, 'index.html', {})
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+@api_view(['GET', 'POST'])
+def menu(request):
+
+    if request.method == 'GET':
+        items = Menu.objects.all()
+        serializer = MenuSerializer(items, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = MenuSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def single_menu_item(request, pk):
+
+    try:
+        item = Menu.objects.get(pk=pk)
+    except Menu.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = MenuSerializer(item)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = MenuSerializer(item, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET', 'POST'])
+def bookings(request):
+
+    if request.method == 'GET':
+        items = Booking.objects.all()
+        serializer = BookingSerializer(items, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = BookingSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def booking(request, pk):
+
+    try:
+        item = Booking.objects.get(pk=pk)
+    except Booking.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = BookingSerializer(item)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = BookingSerializer(item, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
